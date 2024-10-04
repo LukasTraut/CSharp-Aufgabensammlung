e1c7cdb8ee25032b9ae8aa408981a8ae67a06ed0
LukasTraut <lukastraut@hotmail.com>
2024-10-04 09:27:26 +0200

    Aufgabe 14 fertig

---
+++ Zahlen raten/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zahlen_raten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
